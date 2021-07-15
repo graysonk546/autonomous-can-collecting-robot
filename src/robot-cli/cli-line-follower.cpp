0f5bd9513e359f1e21438c6936584eb28d5e66a7
Grayson King <graysonk546@gmail.com>
2021-07-15 12:33:43 -0700

    added line follower cli set threshold function

--- src/robot-cli/cli-line-follower.cpp
+++ src/robot-cli/cli-line-follower.cpp
@@ -68,6 +68,25 @@ cli_status_t cliLineFollower_init(uint8_t argNumber, char* args[])
 
 cli_status_t cliLineFollower_setThreshold(uint8_t argNumber, char* args[])
 {
+    line_follower_id_t lineFollowerId = (line_follower_id_t) strtol(
+                                        (const char*) args[0], NULL, 0);                                    
+
+    if (lineFollowerId > LEFT_LINE_FOLLOWER ||
+        lineFollowerId < RIGHT_LINE_FOLLOWER)
+    {
+        Serial.print(Serial.print(F(CMD_JSON "{\"status\": \"error\", \"data\":"
+                                  " \"invalid line follower\"}" CMD_EOL_STR)));
+        return COMMAND_OK;
+    }
+
+    line_follower_t* lineFollower = lineFollower_get(lineFollowerId);
+
+    lineFollower->threshold = (uint16_t) strtol(
+                                        (const char*) args[1], NULL, 0);  
+
+    Serial.print(Serial.print(F(CMD_JSON "{\"status\": \"success\"}" 
+                                CMD_EOL_STR)));
+    
     return COMMAND_OK;
 }
 
--- src/robot-cli/cli-line-follower.h
+++ src/robot-cli/cli-line-follower.h
@@ -38,7 +38,8 @@ cli_status_t cliLineFollower_setThreshold(uint8_t argNumber, char* args[]);
 
 cli_status_t cliLineFollower_getThreshold(uint8_t argNumber, char* args[]);
 
-#define LINE_FOLLOWER_COMMANDS                                                                                             \
-    {cliLineFollower_init,          "line-follow-init",       "<periph>",          "init the line follower periph", 1, 1}, \
-    {cliLineFollower_setThreshold,  "line-follow-set-thresh", "<periph> <thresh>", "run the 2D dc motor",           2, 2},
+#define LINE_FOLLOWER_COMMANDS                                                                                              \
+    {cliLineFollower_init,          "line-follow-init",        "<periph>",          "init the line follower periph", 1, 1}, \
+    {cliLineFollower_getThreshold,  "line-follow-get-thresh",  "<periph>",          "get line follower threshold",   1, 1}, \
+    {cliLineFollower_setThreshold,  "line-follow-set-thresh",  "<periph> <thresh>", "set line follower threshold",   2, 2},
 #endif // CLI_
